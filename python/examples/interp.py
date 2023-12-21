d268eeebff666e595ad040284c398afc9570773d
daniel_hucik <34608205+hucik14@users.noreply.github.com>
2023-12-21 17:25:44 +0100

    refactor script as class

--- python/examples/interp.py
+++ python/examples/interp.py
@@ -8,6 +8,8 @@ from Plotter import Plotter
 from FactorizationProvider import FactorizationProvider
 from RationalMechanism import RationalMechanism
 
+from MotionInterpolation import MotionInterpolation
+
 
 x = sp.symbols("x:4")
 y = sp.symbols("y:4")
@@ -15,50 +17,50 @@ lam = sp.symbols("lam")
 t = sp.symbols("t:4")
 
 P1 = DualQuaternion.as_rational()
-P2 = DualQuaternion.as_rational([3135, 31350, 6270, 10450, 4296, -4065, -3948, 13275])
-P3 = DualQuaternion.as_rational([-2/5, 1/5, -3/2, 4, 1409/5535, 5753/5535, 998/369, 5467/5535])
-P4 = DualQuaternion.as_rational([2, 1/2, 5/2, -3, -3/130, -289/520, -3/104, -103/780])
 
-P1 = DualQuaternion.as_rational()
+#P2 = DualQuaternion.as_rational([3135, 31350, 6270, 10450, 4296, -4065, -3948, 13275])
+#P3 = DualQuaternion.as_rational([-2/5, 1/5, -3/2, 4, 1409/5535, 5753/5535, 998/369, 5467/5535])
+#P4 = DualQuaternion.as_rational([2, 1/2, 5/2, -3, -3/130, -289/520, -3/104, -103/780])
+
 P2 = DualQuaternion.as_rational([0, 0, 0, 1, 1, 0, 1, 0])
 P3 = DualQuaternion.as_rational([1, 2, 0, 0, -2, 1, 0, 0])
 P4 = DualQuaternion.as_rational([3, 0, 1, 0, 1, 0, -3, 0])
+
+T2 = TransfMatrix.from_rpy_xyz([0, 90, 0], [1, -2, 2], units="deg")
+#P2 = DualQuaternion.as_rational(T2.matrix2dq())
+T3 = TransfMatrix.from_rpy_xyz([-45, 90, 0], [0, 1, 1], units="deg")
+#P3 = DualQuaternion.as_rational(T3.matrix2dq())
+T4 = TransfMatrix.from_rpy_xyz([90, 45, 0], [2, 1, 0], units="deg")
+#P4 = DualQuaternion.as_rational(T4.matrix2dq())
 
 K = P1.array() + x[0] * P2.array() + x[1] * P3.array() + x[2] * P4.array()
 Kdq = DualQuaternion(K)
 
 eqs = [K[0], K[4], Kdq.norm().array()[4]]
 
-#sol = sp.solve(eqs, x, domain=sp.S.Reals)
-sol = sp.solve(eqs, x)
+sol = sp.solve(eqs, x, domain=sp.S.Reals)
+#sol = sp.solve(eqs, x[0], x[1], x[2])
 
 K = [sp.Expr(el) for el in K]
 
 K1 = [el.subs({x[0]: sol[0][0], x[1]: sol[0][1], x[2]: sol[0][2]}) for el in K]
 
-K1 = DualQuaternion([el.args[0] for el in K1])
-print(K1)
-#div = sp.gcd(*[K1[1].args[0], K1[2].args[0], K1[3].args[0], K1[5].args[0], K1[6].args[0], K1[7].args[0]], domain='RR')
-#K2 = [el.subs({x[0]: sol[1][0], x[1]: sol[1][1], x[2]: sol[1][2], x[3]: sol[1][3]}) for el in K]
+#projective_coordinates = sp.together([el.args[0] for el in K1])
+#common_denominator = sp.lcm([expr.denominator for expr in projective_coordinates])
+#result = [expr * common_denominator for expr in projective_coordinates]
 
-#ii = sp.symbols("ii:8")
-#K1factored = [sp.together(element.subs(x[3], 1)) for i, element in enumerate(K1)]
-#K1factored = [sp.Expr(K1factored[i].args[0] * ii[i]) for i in range(len(K1factored))]
+result = [el.args[0] for el in K1]
+print(result)
 
-#result = sp.together(*K1factored)
+#K1 = DualQuaternion([0, 4, -1, 5, 0, 2, 8, 0])
+#K2 = DualQuaternion([0, 2, 1, 1, 0, 1, -2, 0])
 
-###################################
-
-K1 = DualQuaternion([0, 4, -1, 5, 0, 2, 8, 0])
-K2 = DualQuaternion([0, 2, 1, 1, 0, 1, -2, 0])
-
-P5 = DualQuaternion([lam, 0, 0, 0, 0, 0, 0, 0]) - K1
+K1 = DualQuaternion(result)
 
 study_cond_mat = sp.Matrix([[0,0,0,0,1,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,0,0,1,0],[0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0],[0,1,0,0,0,0,0,0],[0,0,1,0,0,0,0,0],[0,0,0,1,0,0,0,0]])
 
-dq = sp.Matrix([x[0], x[1], x[2], x[3], y[0], y[1], y[2], y[3]])
-
-study_cond = dq.transpose() @ study_cond_mat @ dq
+#dq = sp.Matrix([x[0], x[1], x[2], x[3], y[0], y[1], y[2], y[3]])
+#study_cond = dq.transpose() @ study_cond_mat @ dq
 
 t1 = DualQuaternion([t[1], 0, 0, 0, 0, 0, 0, 0])
 t2 = DualQuaternion([t[2], 0, 0, 0, 0, 0, 0, 0])
@@ -68,10 +70,11 @@ eqs2 = [sp.Matrix((t1 - K1).array()).transpose() @ study_cond_mat @ sp.Matrix(P2
         sp.Matrix((t2 - K1).array()).transpose() @ study_cond_mat @ sp.Matrix(P3.array()),
         sp.Matrix((t3 - K1).array()).transpose() @ study_cond_mat @ sp.Matrix(P4.array())]
 
-solst = sp.solve(eqs2, t)
+solst = sp.solve(eqs2, t[1], t[2], t[3])
 # covert to list
 solst = [val for i, (key, val) in enumerate(solst.items())]
 
+solst2 = MotionInterpolation.interpolate([P2, P3, P4])
 
 def lagrange_polynomial(n, k, x, t):
     # n is the degree of the polynomial
@@ -88,7 +91,6 @@ def interpolation_polynomial(P, x):
     # Interpolation polynomial with indeterminate x and parameter values x_i.
     # P... list of Poses
     n = len(P) - 1
-    #result = sp.Add(*[P[k] * lagrange_polynomial(n, k, x, list(range(n+1))) for k in range(n+1)])
     result = sp.Matrix([0, 0, 0, 0, 0, 0, 0, 0])
     t = sp.symbols("t:4")
     for i in range(n+1):
@@ -109,9 +111,11 @@ temp3 = [sp.together(element * x**3) for element in temp2]
 temp4 = [sp.together(element.subs({t[1]: 1/solst[0], t[2]: 1/solst[1], t[3]: 1/solst[2]}))
          for element in temp3]
 
+P5 = DualQuaternion([lam, 0, 0, 0, 0, 0, 0, 0]) - K1
+
 eqslambda = [element.subs(x, lam) - lams[-1] * P5.array()[i] for i, element in enumerate(temp4)]
 
-solslambda = sp.solve(eqslambda, lams[1:5])
+solslambda = sp.solve(eqslambda, lams[1:5], domain=sp.S.Reals)
 
 Q = [element.subs(solslambda) for element in temp4]
 
@@ -125,7 +129,7 @@ Q_poly = [sp.Poly(element, t) for element in Q]
 
 #print(sp.simplify(Q_as_dq.norm()[4]))
 
-c = RationalCurve(Q_poly)
+c = solst2
 
 p = Plotter(interactive=True, steps=1000)
 #p.plot(c, interval=[-10, 10])
