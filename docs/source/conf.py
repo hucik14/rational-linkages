4a3e132c2a6df703ad32d7f7618dda2e2627fb77
daniel_hucik <34608205+hucik14@users.noreply.github.com>
2024-01-10 17:43:22 +0100

    tests failing update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ copyright = '2023, Daniel'
 author = 'Daniel'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.9'
+release = '0.9.10'
 
 
 # -- General configuration ---------------------------------------------------
