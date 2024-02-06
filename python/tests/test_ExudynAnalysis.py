26804c29c2d1bc610740107ab98cbb7d5eefd417
daniel_hucik <34608205+hucik14@users.noreply.github.com>
2024-02-06 15:35:15 +0100

    empty test added

---
+++ python/tests/test_ExudynAnalysis.py
@@ -0,0 +1,25 @@
+from rational_linkages import ExudynAnalysis
+
+
+def test_get_exudyn_params():
+    pass
+
+
+def test__links_points():
+    pass
+
+
+def test__relative_links_points():
+    pass
+
+
+def test__links_lengths():
+    pass
+
+
+def test__links_center_of_gravity():
+    pass
+
+
+def test__joints_axes():
+    pass
