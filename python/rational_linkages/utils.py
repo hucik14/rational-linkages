9e6480fab1bba6e71966be3658f74d13f2741ee1
daniel_hucik <34608205+hucik14@users.noreply.github.com>
2024-08-12 17:52:55 +0200

    util to calculate sum of squares in the list
    Changelog: added

--- python/rational_linkages/utils.py
+++ python/rational_linkages/utils.py
@@ -1,10 +1,19 @@
-from importlib.metadata import distribution
+# This file contains utility functions that are used in the rational_linkages package.
+
+
+def sum_of_squares(list_of_values: list) -> float:
+    """
+    Calculate the sum of squares of a list of values.
+    """
+    return sum([value**2 for value in list_of_values])
 
 
 def is_package_installed(package_name: str) -> bool:
     """
     Check if a package is installed.
     """
+    from importlib.metadata import distribution
+
     try:
         distribution(package_name)
         return True
