bda2c7de80b2fa3171242eb9b5816971e138f0df
daniel_hucik <34608205+hucik14@users.noreply.github.com>
2025-04-10 15:14:29 +0200

    optimize for added t, reshape

--- python/rational_linkages/MotionApproximation.py
+++ python/rational_linkages/MotionApproximation.py
@@ -39,9 +39,10 @@ class MotionApproximation:
         if init_curve.degree != 3:
             raise ValueError("So far, only cubic curves are supported")
 
+        t_array = np.asarray(t_vals)
         approx_curve, opt_result = MotionApproximation._cubic_approximation(init_curve,
                                                                             poses,
-                                                                            t_vals)
+                                                                            t_array)
 
         return approx_curve, opt_result
 
@@ -55,15 +56,10 @@ class MotionApproximation:
         :return: RationalCurve constructed from the coefficients
         :rtype: RationalCurve
         """
-        coeffs = np.array([np.concatenate(([1], flattended_coeffs[:3]), axis=None),
-                           np.concatenate(([0], flattended_coeffs[3:6]), axis=None),
-                           np.concatenate(([0], flattended_coeffs[6:9]), axis=None),
-                           np.concatenate(([0], flattended_coeffs[9:12]), axis=None),
-                           np.concatenate(([0], flattended_coeffs[12:15]), axis=None),
-                           np.concatenate(([0], flattended_coeffs[15:18]), axis=None),
-                           np.concatenate(([0], flattended_coeffs[18:21]), axis=None),
-                           np.concatenate(([0], flattended_coeffs[21:]), axis=None)
-                           ])
+        coeffs = np.zeros((8, 4))  # Preallocate an array of shape (8, 4)
+        coeffs[0, 0] = 1
+        coeffs[:, 1:] = flattended_coeffs.reshape(8, 3)
+
         return RationalCurve.from_coeffs(coeffs)
 
     @staticmethod
@@ -81,13 +77,15 @@ class MotionApproximation:
                                for pose in poses])
 
         initial_guess = init_curve.coeffs[:,1:4].flatten()
+        initial_guess = np.concatenate((initial_guess, t_vals[-1]), axis=None)
 
         def objective_function(params):
             """
             Objective function to minimize the sum of squared distances between
             the poses and the curve
             """
-            curve = MotionApproximation._construct_curve(params)
+            curve = MotionApproximation._construct_curve(params[:24])
+            t_vals[-1] = params[24:][0]
 
             sq_dist = 0.
             for i, pose in enumerate(poses):
@@ -97,7 +95,7 @@ class MotionApproximation:
             return sq_dist
 
         def constraint_func(params):
-            curve = MotionApproximation._construct_curve(params)
+            curve = MotionApproximation._construct_curve(params[:24])
             sq_err = curve.study_quadric_check()
 
             if len(sq_err) != 6:  # expand if necessary to avoid index errors
@@ -128,6 +126,6 @@ class MotionApproximation:
                           )
 
         print(result)
-        result_curve = MotionApproximation._construct_curve(result.x)
+        result_curve = MotionApproximation._construct_curve(result.x[:24])
 
         return result_curve, result
