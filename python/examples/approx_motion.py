7bac73cbd64f4bafd88b2f0da6679ab1dc703adb
daniel_hucik <34608205+hucik14@users.noreply.github.com>
2025-04-11 10:46:04 +0200

    script update

--- python/examples/approx_motion.py
+++ python/examples/approx_motion.py
@@ -3,7 +3,7 @@ import numpy as np
 from rational_linkages import (
     DualQuaternion,
     MotionInterpolation,
-    Plotter,
+    PlotterPyqtgraph as Plotter,
     RationalMechanism,
     TransfMatrix,
     MotionApproximation,
@@ -17,25 +17,31 @@ p3 = DualQuaternion([3., 0, 1, 0, 1, 0, -3, 0])
 
 c = MotionInterpolation.interpolate([p0, p1, p2, p3])
 
-t_sols = MotionInterpolation._solve_for_t([p0, p1, p2, p3], MotionInterpolation._obtain_k_dq([p0, p1, p2, p3]))
-t_sols2 = MotionInterpolation.solve_for_t_numerically([p0, p1, p2, p3], MotionInterpolation._obtain_k_dq([p0, p1, p2, p3]))
-print(t_sols)
-print(t_sols2)
+# t_sols2 = MotionInterpolation._solve_for_t([p0, p1, p2, p3], MotionInterpolation._obtain_k_dq([p0, p1, p2, p3]))
+# print(t_sols2)
+t_sols = MotionInterpolation._solve_for_t_numerically([p0, p1, p2, p3], MotionInterpolation._obtain_k_dq([p0, p1, p2, p3]))
+# print(t_sols)
 
 add_t = 1.7
 p4 = TransfMatrix(c.evaluate_as_matrix(add_t)) * TransfMatrix.from_rpy_xyz([5, -2, 4], [-0.3, 0, 0.2], unit='deg')
 p4 = DualQuaternion(p4.matrix2dq())
 
+add_t2 = 30
+p5 = TransfMatrix(c.evaluate_as_matrix(add_t2)) * TransfMatrix.from_rpy_xyz([5, -3, 5], [-0.2, 0.3, 0.1], unit='deg')
+p5 = DualQuaternion(p5.matrix2dq())
+
 myinf = 1/np.finfo(float).eps
-ts = np.concatenate((myinf, t_sols, [add_t]), axis=None)
+ts = np.concatenate((myinf, t_sols, [add_t, add_t2]), axis=None)
 
-a, res = MotionApproximation.approximate(c, [p0, p1, p2, p3, p4], ts)
+a, res = MotionApproximation.approximate(c, [p0, p1, p2, p3, p4, p5], ts)
+print(res.x[24:])
 
-p = Plotter(interactive=True, steps=1000, arrows_length=0.5)
+# p = Plotter(interactive=True, steps=1000, arrows_length=0.5)
+p = Plotter(arrows_length=0.5)
 
 if a.is_on_study_quadric():
     m = RationalMechanism(a.factorize())
-    p.plot(m, show_tool=True)
+    p.plot(m.curve(), interval='closed', with_poses=True)
     print("")
     print("Result on Study Quadric")
 else:
@@ -44,7 +50,7 @@ else:
     print("Optimization did not converge (result not on study quadric)")
 
 
-poses_to_plot = [p0, p1, p2, p3, p4]
+poses_to_plot = [p0, p1, p2, p3, p4, p5]
 
 for i, pose in enumerate(poses_to_plot):
     p.plot(pose, label=f"p{i}")
