464eca5f9fc282a9d5eed64ba7884fd7f1fa26b6
daniel_hucik <34608205+hucik14@users.noreply.github.com>
2025-04-25 16:42:40 +0200

    rust backend - library import and test methods
    Changelog: added

---
+++ rust/lib.rs
@@ -0,0 +1,22 @@
+use pyo3::prelude::*;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+#[pyfunction]
+fn sum(a: f64, b: f64) -> PyResult<f64> {
+    Ok(a + b)
+}
+
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
+#[pymodule]
+fn utils_rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(sum, m)?)?;
+    Ok(())
+}
