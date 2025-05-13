c939847eeeb8f0250eb0297776525cf47a0c5a15
daniel_hucik <34608205+hucik14@users.noreply.github.com>
2025-05-13 16:45:12 +0200

    Plotter.py returns a class instance based on given parameters
    Changelog: added

--- python/rational_linkages/Plotter.py
+++ python/rational_linkages/Plotter.py
@@ -3,80 +3,97 @@ from .PlotterPyqtgraph import PlotterPyqtgraph, InteractivePlotter
 
 
 class Plotter:
-    def __init__(self,
-                 mechanism: RationalMechanism = None,
-                 show_tool: bool = True,
-                 backend: str = 'pyqtgraph',
-                 jupyter_notebook: bool = False,
-                 show_legend: bool = False,
-                 show_controls: bool = True,
-                 interval: tuple = (-1, 1),
-                 steps: int = None,
-                 arrows_length: float = 1.0,
-                 joint_sliders_lim: float = 1.0,
-                 white_background: bool = False,
-                 parent_app=None, ):
+    def __new__(cls,
+               mechanism: RationalMechanism = None,
+               show_tool: bool = True,
+               backend: str = 'pyqtgraph',
+               jupyter_notebook: bool = False,
+               show_legend: bool = False,
+               show_controls: bool = True,
+               interval: tuple = (-1, 1),
+               steps: int = None,
+               arrows_length: float = 1.0,
+               joint_sliders_lim: float = 1.0,
+               white_background: bool = False,
+               parent_app=None):
         """
-        Initialize the plotter super class.
+        Create and return an appropriate plotter instance based on parameters.
 
         Default backend is 'pyqtgraph', but can be changed to 'matplotlib'.
         """
+        # delegate to the create method
+        return cls.create(
+            mechanism=mechanism,
+            show_tool=show_tool,
+            backend=backend,
+            jupyter_notebook=jupyter_notebook,
+            show_legend=show_legend,
+            show_controls=show_controls,
+            interval=interval,
+            steps=steps,
+            arrows_length=arrows_length,
+            joint_sliders_lim=joint_sliders_lim,
+            white_background=white_background,
+            parent_app=parent_app
+        )
+    @classmethod
+    def create(cls,
+               mechanism: RationalMechanism = None,
+               show_tool: bool = True,
+               backend: str = 'pyqtgraph',
+               jupyter_notebook: bool = False,
+               show_legend: bool = False,
+               show_controls: bool = True,
+               interval: tuple = (-1, 1),
+               steps: int = None,
+               arrows_length: float = 1.0,
+               joint_sliders_lim: float = 1.0,
+               white_background: bool = False,
+               parent_app=None):
+        """
+        Create and return an appropriate plotter instance based on parameters.
 
-        if mechanism is not None and not jupyter_notebook:
-            interactive = True
-        else:
-            interactive = False
+        Default backend is 'pyqtgraph', but can be changed to 'matplotlib'.
+        """
+        interactive = mechanism is not None and not jupyter_notebook
 
         if backend == 'pyqtgraph' and not jupyter_notebook:
             if show_legend:
                 print('Warning: The legend is supported only in Matplotlib backend. ')
             elif not show_controls:
-                print('Warning: Hiding controls is supported only in Matplotlib backend.')
+                print(
+                    'Warning: Hiding controls is supported only in Matplotlib backend.')
 
             if steps is None:
                 steps = 2000
 
             if interactive:
-                self.plt = InteractivePlotter(mechanism=mechanism,
-                                              show_tool=show_tool,
-                                              steps=steps,
-                                              arrows_length=arrows_length,
-                                              joint_sliders_lim=joint_sliders_lim,
-                                              white_background=white_background)
+                return InteractivePlotter(mechanism=mechanism,
+                                          show_tool=show_tool,
+                                          steps=steps,
+                                          arrows_length=arrows_length,
+                                          joint_sliders_lim=joint_sliders_lim,
+                                          white_background=white_background)
             else:
-                self.plt = PlotterPyqtgraph(parent_app=parent_app,
-                                            interval=interval,
-                                            steps=steps,
-                                            arrows_length=arrows_length,
-                                            white_background=white_background)
-
+                return PlotterPyqtgraph(parent_app=parent_app,
+                                        interval=interval,
+                                        steps=steps,
+                                        arrows_length=arrows_length,
+                                        white_background=white_background)
         else:
             if steps is None:
                 steps = 200
 
             from .PlotterMatplotlib import PlotterMatplotlib
-            self.plt = PlotterMatplotlib(interactive=interactive,
-                                             jupyter_notebook=jupyter_notebook,
-                                             show_legend=show_legend,
-                                             show_controls=show_controls,
-                                             interval=interval,
-                                             steps=steps,
-                                             arrows_length=arrows_length,
-                                             joint_sliders_lim=joint_sliders_lim)
+            plotter = PlotterMatplotlib(interactive=interactive,
+                                        jupyter_notebook=jupyter_notebook,
+                                        show_legend=show_legend,
+                                        show_controls=show_controls,
+                                        interval=interval,
+                                        steps=steps,
+                                        arrows_length=arrows_length,
+                                        joint_sliders_lim=joint_sliders_lim)
             if mechanism is not None:
-                self.plt.plot(mechanism, show_tool=show_tool)
+                plotter.plot(mechanism, show_tool=show_tool)
 
-    def plot(self, *args, **kwargs):
-        """
-        Plot the given arguments.
-
-        :param args: The arguments to plot.
-        :param kwargs: The keyword arguments to plot.
-        """
-        self.plt.plot(*args, **kwargs)
-
-    def show(self):
-        """
-        Show the plot.
-        """
-        self.plt.show()
+            return plotter
