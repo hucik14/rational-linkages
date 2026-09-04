384ee85d80a1bcfe47180bbbb11b7f2b03d383b4
daniel_hucik <34608205+hucik14@users.noreply.github.com>
2026-09-04 10:25:05 +0900

    bennett example update

--- docs/source/examples/d_ark2024_bennett.py
+++ docs/source/examples/d_ark2024_bennett.py
@@ -1,5 +1,11 @@
-from rational_linkages import RationalCurve, RationalMechanism, FactorizationProvider, Plotter
+from rational_linkages import RationalCurve, RationalMechanism, Plotter, DualQuaternion, NormalizedLine
 import numpy as np
+
+
+# input poses (not used for calculation, only for plotting)
+p0 = DualQuaternion([0, 17, -33, -89, 0, -6, 5, -3])
+p1 = DualQuaternion([0, 84, -21, -287, 0, -30, 3, -9])
+p2 = DualQuaternion([0, 10, 37, -84, 0, -3, -6, -3])
 
 coeffs = np.array([[0, 0, 0],
                    [4440, 39870, 22134],
@@ -13,15 +19,22 @@ coeffs = np.array([[0, 0, 0],
 # define a rational curve object
 c = RationalCurve.from_coeffs(coeffs)
 
-# factorize the curve
-factors = c.factorize()
+# define a mechanism object from curve factorization
+m = RationalMechanism(c.factorize())
 
-# define a mechanism object
-m = RationalMechanism(factors)
+# define a plotter object, set mechanism, backend, number of discrete steps, frame arrows length,
+# and base pose - the base pose is used change the reference frame of the mechanism,
+# according to the methodology, it equals p2
+p = Plotter(mechanism=m, backend='matplotlib', steps=500, arrows_length=0.05, base=p2)
 
-# define a plotter object, set interactive mode and number of discrete steps
-# to plot the curve
-p = Plotter(mechanism=m, steps=500, arrows_length=0.05)
+# plot poses
+p.plot(DualQuaternion(), label='world (identity)')
+p.plot(p0, label='a')
+p.plot(p1, label='b')
+p.plot(p2, label='c')
+
+# la = NormalizedLine(p2.dq2screw())
+# p.plot(la, label='l_a', interval=(-0.1, 0.1), color='orange')
 
 # plot the mechanism
 p.show()
